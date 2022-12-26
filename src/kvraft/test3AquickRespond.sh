8408a7f61f1791bf8ce9c5ed5a79cd35044ba5f5
530373305@qq.com <530373305@qq.com>
2022-12-25 21:08:37 -0800

    modified:   src/kvraft/client.go 	modified:   src/kvraft/server.go 	modified:   src/raft/append.go 	modified:   src/raft/persist.go 	modified:   src/raft/raft.go 	modified:   src/raft/snapshot.go 	modified:   src/raft/util.go

---
+++ src/kvraft/test3AquickRespond.sh
@@ -0,0 +1,6 @@
+#for i in {1..10000}; do go test  -race; done
+count=1000
+    for i in $(seq $count); do
+        printf "****************************************\n"
+        go test -race
+    done
